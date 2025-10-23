efc8ac53e8ccce2ff8ffc11a3ccfdf9bf8962a6a
project-AI39 <project.ai39@gmail.com>
2025-10-23 11:34:44 +0900

    🔧 chore: マイグレーションのリセットのためファイルを削除

--- web/prisma/migrations/20250828153540_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Event" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
-);
